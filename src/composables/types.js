fb670923253e667c442508bcb98d5eedfa46600a
Olga Bulat <obulat@gmail.com>
2021-12-14 17:51:55 +0300

    Fix assets import links

--- src/composables/types.js
+++ src/composables/types.js
@@ -8,4 +8,8 @@
  * @typedef {import('@nuxtjs/composition-api').ToRefs<T>} ToRefs
  */
 
+/** @typedef {{ id: string, name: string, link: string|*, icon?: string }} PageObject */
+
+/** @typedef {{ pages: PageObject[], currentPageId: import('@nuxtjs/composition-api').ComputedRef<string> }} UsePages */
+
 export default {}
